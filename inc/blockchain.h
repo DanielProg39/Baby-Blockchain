f921d2e48b377f456a8dd8732190d40a1ae9e853
Daniel <63293432+DanielProg39@users.noreply.github.com>
2023-02-12 23:19:09 +0200

    Stage 2
    Added KeyPair and Signature classes

---
+++ inc/blockchain.h
@@ -0,0 +1,19 @@
+#ifndef BLOCKCHAIN_H
+#define BLOCKCHAIN_H
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <openssl/pem.h>
+
+// Following are used primarily for testing, may be removed later
+
+// #include <cstdio>
+// #include <openssl/ec.h>
+// #include <openssl/obj_mac.h>
+// #include <openssl/pem.h>
+// #include <openssl/err.h>
+// #include <openssl/ecdsa.h>
+// #include <openssl/bn.h>
+
+#endif
