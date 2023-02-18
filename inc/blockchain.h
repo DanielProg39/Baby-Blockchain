aad1a7028295083d8a3b21a912ffa8df87bb57fc
Daniel <63293432+DanielProg39@users.noreply.github.com>
2023-02-18 12:13:41 +0200

    Stage 3
    This header is not completely ready, it will be re-written after the most essential work will be done. For now, it is simple, convenient and works properly

--- inc/blockchain.h
+++ inc/blockchain.h
@@ -2,18 +2,20 @@
 #define BLOCKCHAIN_H
 
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <cstring>
 #include <openssl/pem.h>
+#include <openssl/ec.h>
 
 // Following are used primarily for testing, may be removed later
 
 // #include <cstdio>
-// #include <openssl/ec.h>
 // #include <openssl/obj_mac.h>
 // #include <openssl/pem.h>
 // #include <openssl/err.h>
 // #include <openssl/ecdsa.h>
 // #include <openssl/bn.h>
+// #include <algorithm>
 
 #endif
